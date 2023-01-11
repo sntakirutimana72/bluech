b25f8aecc6cd04a0c144c51c23b0257e76ba14a5
Steven Ntakirutimana <sntakirutimana72@gmail.com>
2023-01-11 16:13:31 +0000

    Add sql queries actions

---
+++ app/utils/sql.py
@@ -0,0 +1,22 @@
+from ..models import User
+
+def signin(username):
+    user = User.get_or_none(User.name == username)
+    return user.id if user else None
+    
+def signout(user_id):
+    ...
+    
+def change_user_display_name(username, display_name):
+    user = User.get_or_none(User.name = username)
+    if user is None:
+        return
+    if user.display_name == display_name or User.get_or_none(User.display_name == display_name):
+        return
+    user.display_name = display_name
+    user.save()
+    
+    return user.id
+    
+def 
+    

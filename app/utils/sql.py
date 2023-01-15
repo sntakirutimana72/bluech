49e03128f1d995cd53e8d6717be2ece344f8754a
Steven Ntakirutimana <sntakirutimana72@gmail.com>
2023-01-15 11:33:35 +0000

    Update db queries

--- app/utils/sql.py
+++ app/utils/sql.py
@@ -1,18 +1,23 @@
 from .exceptions import Unauthorized
-from ..models import User
+from ..models import User, Log
+from ..settings import LOGGING_LEVELS
 
-def signin(username):
+def db_logger(**kwargs) -> int:
+    return Log.create(**kwargs)
+
+def signin(username) -> User:
     try:
         user = User.get(User.name == username)
     except:
         raise Unauthorized
-
+        
+    db_logger(action_id=LOGGING_LEVELS.LOGIN, done_by=user)
     return user
 
-def signout():
-    ...
+def signout(user_id):
+    db_logger(action_id=LOGGING_LEVELS.LOGOUT, done_by=user_id)
 
-def change_user_display_name(username, display_name):
+def change_user_display_name(username, display_name) -> int:
     user = User.get_or_none(User.name == username)
     if user is None:
         return

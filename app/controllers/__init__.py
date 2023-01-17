a6aabd2f79c5ce82ae44564be929c109baa1ab41
Steven Ntakirutimana <sntakirutimana72@gmail.com>
2023-01-17 22:14:56 +0000

    Update the controllers module index entry file

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -1,3 +1,13 @@
+__all__ = (
+    'BaseController',
+    'UsersController',
+    'SessionController',
+    'MessagesController',
+    'GroupsController',
+)
+
 from .base import Base as BaseController
 from .session import Session as SessionController
 from .messages import Messages as MessagesController
+from .users import Users as UsersController
+from .groups import Groups as GroupsController

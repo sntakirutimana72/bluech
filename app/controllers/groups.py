4884f3d17cfb826cb2bf22d2d7a3f2106e987daa
Steven Ntakirutimana <sntakirutimana72@gmail.com>
2023-01-18 17:30:59 +0200

    Update groups.py

--- app/controllers/groups.py
+++ app/controllers/groups.py
@@ -17,9 +17,18 @@ class Groups(Base):
 
     async def _pop(self):
         """Remove a group member"""
+        params = self._request.params
+        sql.remove_member(self.user_id, **params)
+        await create_response_task(self._request.protocol, None, **params)
 
     async def _drop(self):
         """Exit group"""
+        params = {'member_id': self.user_id, 'group_id': self._request.params.id}
+        sql.exit_group(**params)
+        await create_response_task(self._request.protocol, None, **params)
 
     async def _delete(self):
         """Delete a group"""
+        pk = self._request.params.id
+        sql.delete_group(self.user_id, pk)
+        await create_response_task(self._request.protocol, pk)

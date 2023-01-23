b8de0c58e23b24f8d9b1d577b02a79665265eb1b
Steve <sntakirutimana72@gmail.com>
2023-01-23 12:12:30 +0200

    Update db_connect.py

--- app/utils/db_connect.py
+++ app/utils/db_connect.py
@@ -4,15 +4,40 @@ import psycopg2
 from ..settings import DB_CONFIGS
 from ..models import *
 
+def _init_conn(**options):
+    con = psycopg2.connect(**options)
+    con.set_session(autocommit=True)
+    return con
+
+# noinspection PyBroadException
+def create_schema(schema, **options):
+    con = None
+    try:
+        con = _init_conn(**options)
+        con.cursor().execute(f'CREATE DATABASE {schema}')
+    except:
+        ...
+    if con:
+        con.close()
+
+# noinspection PyBroadException
+def drop_schema(schema, **options):
+    con = None
+    try:
+        con = _init_conn(**options)
+        con.cursor().execute(f'DROP DATABASE {schema}')
+    except:
+        ...
+    if con:
+        con.close()
+
 def db_connector(env='development'):
     """ Used to set up and load database initial configuration on start-up. """
     # Ready options
     options = DB_CONFIGS[env].copy()
     schema = options.pop('schema')
     # Ensure schema exists
-    _ = psycopg2.connect(**options)
-    _.cursor().execute(f'CREATE DATABASE {schema!r}')
-    _.close()
+    create_schema(schema, **options)
     # Create a db instance with specific configurations
     database = pee.PostgresqlDatabase(schema, **options)
     # Initiate db instance connection

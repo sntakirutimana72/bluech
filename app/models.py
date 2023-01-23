35393fdd1858bd52714d5b6378f80516733ad0f1
Steve <sntakirutimana72@gmail.com>
2023-01-23 12:11:50 +0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -28,28 +28,26 @@ class _Model(Model):
         database = DatabaseProxy()
 
     def as_json(self):
-        raise NotImplementedError
+        ...
 
     @property
     def name(self):
         return self.__class__.__name__.lower()
-    
+
 class User(_Model):
     email = CharField(unique=True, max_length=100)
     password = CharField(null=False)
     nickname = CharField(max_length=12, null=False)
 
     def as_json(self):
-        return AttributeDict({
-            'id': self.id, 'nickname': self.nickname, 'photo': self.photo.to_json()
-        })
-        
+        return AttributeDict({'id': self.id, 'nickname': self.nickname})
+
 class Channel(_Model):
     uuid = UUIDField(unique=True, default=uuid4)
     nickname = CharField(max_length=12, null=False)
     created_by = ForeignKeyField(User, backref='channels')
     members = ManyToManyField(User, backref='channels', through_model=MembershipThroughModel)
-    
+
 class Member(_Model):
     is_admin = BooleanField(default=False, null=False)
     is_creator = BooleanField(default=False, null=False)
@@ -58,25 +56,25 @@ class Member(_Model):
 
     class Meta:
         primary_key = CompositeKey('user', 'channel')
-        
+
 MembershipThroughModel.set_model(Member)
-        
+
 class Message(_Model):
     sender = ForeignKeyField(User, backref='messages')
     recipient = ForeignKeyField(User, backref='receipts')
     description = TextField()
     is_edited = BooleanField(default=False, null=False)
     reply_to = ForeignKeyField('self', backref='replies', null=True)
-    
+
 class Resource(_Model):
     name = CharField(max_length=60)
     path = CharField(unique=True)
     suffix = CharField(max_length=5)
-    
+
     user = ForeignKeyField(User, backref='picture', null=True)
     channel = ForeignKeyField(Channel, backref='attachments', null=True)
     message = ForeignKeyField(Message, backref='attachments', null=True)
-    
+
     def to_json(self):
         return AttributeDict({
             'name': self.name, 'path': self.path, 'suffix': self.suffix

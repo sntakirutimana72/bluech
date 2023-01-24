63025d5fcae5f096b1d0b41228eb117d8c445dcd
Steven Ntakirutimana <sntakirutimana72@gmail.com>
2023-01-24 21:26:55 +0000

    Update conftest

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,17 +1,18 @@
 import pytest
+import peewee as pee
 
-from ..utils.db_connect import db_connector, drop_schema
-from ..settings import DB_CONFIGS
+from ..utils.db_connect import db_connector
+from .. import models
 
-@pytest.fixture
+@pytest.fixture(scope='session')
 def configure_db():
     conn = db_connector('test')
     
     yield conn
+    model_cls: list[pee.Model] = list(map(models.__dict__.get, models.__all__))  # type: ignore
+    model_cls.reverse()
     
+    for cls in model_cls:
+        cls.truncate_table(restart_identity=True, cascade=True)
+        
     conn.close()
-    
-    options = DB_CONFIGS['test'].copy()
-    schema = options.pop('schema')
-    
-    drop_schema(schema, **options)

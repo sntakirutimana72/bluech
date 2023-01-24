f56c36fed39646b14f5d4125a8d74037af2f8dfd
Steven Ntakirutimana <sntakirutimana72@gmail.com>
2023-01-24 21:31:25 +0000

    Update user test units

--- app/tests/models/test_user.py
+++ app/tests/models/test_user.py
@@ -23,26 +23,26 @@ class UserTestCases(unit.TestCase):
     def test_valid_user(self):
         self.assertIsInstance(self.user, User)
 
-    # def test_email_uniqueness(self):
-    #     with self.assertRaises(pee.IntegrityError):
-    #         new_user()
+    def test_email_uniqueness(self):
+        with self.assertRaises(pee.IntegrityError):
+            new_user()
 
-    # def test_email_cannot_be_null(self):
-    #     with self.assertRaises(pee.IntegrityError):
-    #         new_user(email=None)
+    def test_email_cannot_be_null(self):
+        with self.assertRaises(pee.IntegrityError):
+            new_user(email=None)
             
-    # def test_email_cannot_be_more_than_100(self):
-    #     with self.assertRaises(pee.DataError):
-    #         new_user(email=('johnde@'*15))
+    def test_email_cannot_be_more_than_100(self):
+        with self.assertRaises(pee.DataError):
+            new_user(email=('johnde@'*15))
             
-    # def test_nickname_cannot_be_null(self):
-    #     with self.assertRaises(pee.IntegrityError):
-    #         new_user(email="noway@", nickname=None)
+    def test_nickname_cannot_be_null(self):
+        with self.assertRaises(pee.IntegrityError):
+            new_user(email="noway@", nickname=None)
     
-    # def test_nickname_cannot_be_more_than_12(self):
-    #     with self.assertRaises(pee.DataError):
-    #         new_user(email="noway1@", nickname='cdsakhoer903228jgwe098')
+    def test_nickname_cannot_be_more_than_12(self):
+        with self.assertRaises(pee.DataError):
+            new_user(email="noway1@", nickname='cdsakhoer903228jgwe098')
             
-    # def test_password_cannot_be_null(self):
-    #     with self.assertRaises(pee.IntegrityError):
-    #         new_user(email="noway3@", password=None)
+    def test_password_cannot_be_null(self):
+        with self.assertRaises(pee.IntegrityError):
+            new_user(email="noway3@", password=None)

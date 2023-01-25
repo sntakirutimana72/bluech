be2187c477afe71cd0cfe1f3eda4700927219fc6
Steven Ntakirutimana <sntakirutimana72@gmail.com>
2023-01-25 20:51:06 +0000

    Update user model tests to properly interact with testing support utils

--- app/tests/models/test_user.py
+++ app/tests/models/test_user.py
@@ -2,18 +2,8 @@ import unittest as unit
 import peewee as pee
 import pytest
 
-from ...models import *
-
-def new_user(**options):
-    user = User.create(**{
-        'email': 'admin@email.com', 'password': '1234', 'nickname': 'josh567',
-        **options
-    })
-    return user
-
-@pytest.fixture(scope='class')
-def user(request):
-    request.cls.user = new_user()
+from ..support.helpers import create_user
+from ...models import User
 
 @pytest.mark.usefixtures('configure_db', 'user')
 class UserTestCases(unit.TestCase):
@@ -27,24 +17,24 @@ class UserTestCases(unit.TestCase):
 
     def test_email_uniqueness(self):
         with self.assertRaises(pee.IntegrityError):
-            new_user()
+            create_user()
 
     def test_email_cannot_be_null(self):
         with self.assertRaises(pee.IntegrityError):
-            new_user(email=None)
+            create_user(email=None)
 
     def test_email_cannot_be_more_than_100(self):
         with self.assertRaises(pee.DataError):
-            new_user(email=('johnde@'*15))
+            create_user(email=('johnde@'*15))
 
     def test_nickname_cannot_be_null(self):
         with self.assertRaises(pee.IntegrityError):
-            new_user(email="noway@", nickname=None)
+            create_user(email="noway@", nickname=None)
 
     def test_nickname_cannot_be_more_than_12(self):
         with self.assertRaises(pee.DataError):
-            new_user(email="noway1@", nickname='cdsakhoer903228jgwe098')
+            create_user(email="noway1@", nickname='cdsakhoer903228jgwe098')
 
     def test_password_cannot_be_null(self):
         with self.assertRaises(pee.IntegrityError):
-            new_user(email="noway3@", password=None)
+            create_user(email="noway3@", password=None)

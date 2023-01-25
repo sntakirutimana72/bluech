edcda005ab8d274e6f3ab3bcda87db220ae4ae1a
Steve <sntakirutimana72@gmail.com>
2023-01-25 11:27:09 +0200

    Add test unit for :as_json method

--- app/tests/models/test_user.py
+++ app/tests/models/test_user.py
@@ -6,13 +6,11 @@ from ...models import *
 
 def new_user(**options):
     user = User.create(**{
-        'email': 'admin@email.com',
-        'password': '1234',
-        'nickname': 'josh567',
+        'email': 'admin@email.com', 'password': '1234', 'nickname': 'josh567',
         **options
     })
     return user
-    
+
 @pytest.fixture(scope='class')
 def user(request):
     request.cls.user = new_user()
@@ -23,6 +21,10 @@ class UserTestCases(unit.TestCase):
     def test_valid_user(self):
         self.assertIsInstance(self.user, User)
 
+    def test_as_json(self):
+        user_json = {'id': self.user.id, 'nickname': self.user.nickname}
+        self.assertDictEqual(self.user.as_json(), user_json)
+
     def test_email_uniqueness(self):
         with self.assertRaises(pee.IntegrityError):
             new_user()
@@ -30,19 +32,19 @@ class UserTestCases(unit.TestCase):
     def test_email_cannot_be_null(self):
         with self.assertRaises(pee.IntegrityError):
             new_user(email=None)
-            
+
     def test_email_cannot_be_more_than_100(self):
         with self.assertRaises(pee.DataError):
             new_user(email=('johnde@'*15))
-            
+
     def test_nickname_cannot_be_null(self):
         with self.assertRaises(pee.IntegrityError):
             new_user(email="noway@", nickname=None)
-    
+
     def test_nickname_cannot_be_more_than_12(self):
         with self.assertRaises(pee.DataError):
             new_user(email="noway1@", nickname='cdsakhoer903228jgwe098')
-            
+
     def test_password_cannot_be_null(self):
         with self.assertRaises(pee.IntegrityError):
             new_user(email="noway3@", password=None)

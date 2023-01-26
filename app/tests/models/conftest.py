5ee042c96302999f2ff4954eb1818fd240e8805f
Steve <sntakirutimana72@gmail.com>
2023-01-26 10:53:24 +0200

    Update testing support utils

--- app/tests/models/conftest.py
+++ app/tests/models/conftest.py
@@ -1,6 +1,6 @@
 import pytest
 
-from ..support.helpers import create_user, create_channel, create_member
+from ..support.helpers import create_user
 
 @pytest.fixture(scope='class')
 def user(request):
@@ -8,10 +8,3 @@ def user(request):
     request.cls.user = _user
     yield _user
     _user.delete_instance()
-    
-@pytest.fixture(scope='class')
-def channel(request, user):
-    _channel = create_channel(created_by=user)
-    request.cls.channel = _channel
-    yield _channel
-    _channel.delete_instance()
--- app/tests/support/helpers.py
+++ app/tests/support/helpers.py
@@ -1,21 +1,30 @@
 import contextlib
 
 from .declarations import Models
-from ...models import User, Channel, Member
+from ...models import User, Channel, Member, Message
 
 def create_user(**options) -> User:
     user = User.create(**Models.user(**options))
     return user
 
 def create_channel(**options) -> Channel:
-    channel =  Channel.create(**Models.channel(**options))
+    channel = Channel.create(**Models.channel(**options))
     return channel
 
 def create_member(**options):
     return Member.create(**options)
+
+def create_message(**options):
+    return Message.create(**options)
 
 @contextlib.contextmanager
 def instant_member(**options):
     member = create_member(**options)
     yield member
     member.delete_instance()
+
+@contextlib.contextmanager
+def instant_message(**options):
+    message = create_message(**options)
+    yield message
+    message.delete_instance()

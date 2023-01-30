df247cb6f3a9a7b0b3b7f747d36064be0e8e35d9
Steve <sntakirutimana72@gmail.com>
2023-01-30 12:31:16 +0200

    Move & resolve pytest fixtures

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+import asyncio
 
 from ..utils.db_connect import db_connector, drop_schema
 from ..settings import DB_CONFIGS
@@ -9,3 +10,9 @@ def configure_db():
     conn = db_connector('test')
     yield conn
     conn.close()
+
+@pytest.fixture(scope='module')
+def event_loop():
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
--- app/tests/test_connectivity.py
+++ app/tests/test_connectivity.py
@@ -4,12 +4,6 @@ import asyncio
 from .support.mocks.server import ConnectivityMockServer
 
 @pytest.fixture(scope='module')
-def event_loop():
-    loop = asyncio.get_event_loop_policy().new_event_loop()
-    yield loop
-    loop.close()
-
-@pytest.fixture(scope='module')
 def server(event_loop):
     _server = ConnectivityMockServer()
     event_loop.run_until_complete(_server.initiate())

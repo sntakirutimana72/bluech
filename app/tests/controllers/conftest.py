ac23606b1247715cb298d98f3e253bb25222a0b0
Steve <sntakirutimana72@gmail.com>
2023-02-10 13:04:41 +0200

    Update all conftest files

--- app/tests/controllers/conftest.py
+++ app/tests/controllers/conftest.py
@@ -8,3 +8,7 @@ from ...utils.middlewares import accept_conn
 def server(event_loop):
     s = event_loop.run_until_complete(asyncio.start_server(accept_conn, HOST_URL, HOST_PORT))
     return s
+
+@pytest.fixture(scope='class')
+def connect_sclient():
+    return asyncio.open_connection(host=HOST_URL, port=HOST_PORT)

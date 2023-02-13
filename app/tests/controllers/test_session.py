d8d7bada4922c776ab09cc68ab1e70f76ab45305
Steven Ntakirutimana <sntakirutimana72@gmail.com>
2023-02-13 23:53:56 +0200

    Update test_session.py

--- app/tests/controllers/test_session.py
+++ app/tests/controllers/test_session.py
@@ -5,8 +5,28 @@ from ..support.unittests import PyTestCase
 class TestSession(PyTestCase):
 
     @pytest.mark.asyncio
-    async def test_signin(self):
-        ...
+    async def test_signin(self, cli_con, cli_discon):
+        reader, writer = await cli_con()
+        signin_req = {
+            'content_length': int,
+            'content_type': 'json',
+            'protocol': 'signin',
+            'request': {
+                'body': {
+                    'user': {
+                        'email': 'admin@email.com',
+                        'password': 'test@123'
+                    }
+                }
+            }
+        }
+        # formatting data and packing it for a specific factory pattern recognizable by the server
+        to_json = json.dumps(signin_req)
+        to_bytes = to_json.encode()
+        c_len = str(len(to_bytes)).encode()
+        payload = c_len + to_bytes
+        writer.write(payload)
+        await writer.drain()
 
     @pytest.mark.asyncio
     async def test_signout(self):

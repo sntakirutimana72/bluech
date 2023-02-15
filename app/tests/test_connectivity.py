79eea185427e73ab82fee4527069e7d081469baa
Steve <sntakirutimana72@gmail.com>
2023-02-15 11:50:19 +0200

    Refactor test_connectivity.py

--- app/tests/test_connectivity.py
+++ app/tests/test_connectivity.py
@@ -9,11 +9,25 @@ def server(event_loop):
     event_loop.run_until_complete(_server.initiate())
     return _server
 
+@pytest.fixture
+def fcli_con():
+    def connect(host='localhost', port=8080):
+        return asyncio.open_connection(host=host, port=port)
+    return connect
+
+@pytest.fixture
+def fcli_discon():
+    async def disconnect(pipe):
+        pipe.close()
+        await pipe.wait_closed()
+    return disconnect
+
+
 @pytest.mark.asyncio
-async def test_client_connection(server, disconnect_client, connect_client):
+async def test_client_connection(server, fcli_con, fcli_discon):
     assert server.con_counter == 0
-    reader, writer = await connect_client()
+    reader, writer = await fcli_con()
     tls = await reader.read()
     assert tls == b'helo'
     assert server.con_counter != 0
-    await disconnect_client(writer)
+    await fcli_discon(writer)

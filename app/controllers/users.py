51c3dd999812d9c0ec0637baac3f19e13d9358a3
Steven Ntakirutimana <sntakirutimana72@gmail.com>
2023-02-17 00:43:22 +0200

    Update users.py

--- app/controllers/users.py
+++ app/controllers/users.py
@@ -1,6 +1,6 @@
 from .base import Base
 from ..utils import sql
-from ..utils.pipe import create_response_task
+from ..utils.layers import TasksLayer
 
 class Users(Base):
 
@@ -8,15 +8,15 @@ class Users(Base):
         """Edit user display name"""
         pk = self.user_id
         sql.edit_user_display_name(pk, self._request.body.display_name)
-        await create_response_task(self._request.protocol, pk)
+        await TasksLayer.build(self._request.protocol, pk)
 
     async def _put(self):
         """Change user profile picture"""
         pk = self.user_id
         body = self._request.body
         sql.edit_user_profile_picture(pk, body.data, body.extension)
-        await create_response_task(self._request.protocol, pk)
+        await TasksLayer.build(self._request.protocol, pk)
 
     async def _get(self):
         """Fetch all current users and groups that you're associated to"""
-        await create_response_task(self._request.protocol, self.user_id)
+        await TasksLayer.build(self._request.protocol, self.user_id)

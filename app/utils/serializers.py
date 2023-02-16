9926484f8873208add5a3da70de467430b345ef4
Steven Ntakirutimana <sntakirutimana72@gmail.com>
2023-02-17 00:55:38 +0200

    Update serializers.py

--- app/utils/serializers.py
+++ app/utils/serializers.py
@@ -3,22 +3,32 @@ import typing
 
 from ..settings import ENCODING
 
-def json_parse(data_payload):
-    return json.loads(data_payload)
-
-def json_stringify(serializable_payload):
-    return json.dumps(serializable_payload)
-
-def from_bytes(data: bytes):
-    return data.decode(ENCODING)
-
-def to_bytes(data: typing.Any):
-    return data.encode(ENCODING)
-
-def decompress(request: bytes):
-    as_string = from_bytes(request)
-    return json_parse(as_string)
-
-def compress(response: typing.Dict[str, typing.Any]):
-    as_string = json_stringify(response)
-    return to_bytes(as_string)
+class PayloadJSONSerializer:
+    @staticmethod
+    def as_json(data_obj: str):
+        return json.loads(data_obj)
+    
+    @staticmethod
+    def as_string(data_obj: dict):
+        return json.dumps(data_obj)
+    
+    @staticmethod
+    def from_bytes(data_obj: bytes):
+        return data_obj.decode(ENCODING)
+    
+    @staticmethod
+    def as_bytes(data_obj: str):
+        return data_obj.encode(ENCODING)
+    
+    @classmethod
+    def decompress(cls, payload: bytes):
+        as_str = cls.from_bytes(payload)
+        return cls.as_json(as_str)
+    
+    @classmethod
+    def compress(cls, obj: typing.Dict[str, typing.Any]):
+        as_str = cls.as_string(obj)
+        as_byte = cls.as_bytes(as_str)
+        content_size = len(as_byte)
+        content = cls.as_bytes(str(content_size)) + as_byte
+        return content

c764c3834a632709ebae927f0015c0685099957b
Steven Ntakirutimana <sntakirutimana72@gmail.com>
2023-02-17 23:21:26 +0200

    Update controllers

--- app/controllers/base.py
+++ app/controllers/base.py
@@ -3,12 +3,11 @@ import asyncio
 from ..utils.interfaces import Request
 
 class Base(object):
-
     def __init__(self, request: Request):
         self._request = request
         
     @property
-    def user_id(self):
+    def user_id(self) -> int:
         return self.session.user_id
     
     @property

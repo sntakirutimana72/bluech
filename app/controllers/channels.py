9e5acdab5fc927f21786afd5450ed97ddb08e5ed
Steven Ntakirutimana <sntakirutimana72@gmail.com>
2023-02-17 23:21:53 +0200

    Update controllers

--- app/controllers/channels.py
+++ app/controllers/channels.py
@@ -1,34 +1,33 @@
 from .base import Base
-from ..utils import sql
+from ..utils.sql import ChannelQueryManager
 from ..utils.layers import TasksLayer
 
 class Channels(Base):
-
     async def _post(self):
         """Create a new channel"""
-        channel_id = sql.new_channel(self.user_id, **self._request.body)
-        await TasksLayer.build(self._request.protocol, channel_id)
+        pk = ChannelQueryManager.new_channel(self.user_id, **self._request.body)
+        await TasksLayer.build(self._request.protocol, pk)
 
     async def _put(self):
         """Add new member in the channel"""
         pk = self._request.params.id
-        options = sql.new_member(self.user_id, pk, **self._request.body)
+        options = ChannelQueryManager.new_member(self.user_id, pk, **self._request.body)
         await TasksLayer.build(self._request.protocol, None, **options)
 
     async def _pop(self):
         """Remove a channel member"""
         params = self._request.params
-        sql.remove_member(self.user_id, **params)
+        ChannelQueryManager.remove_member(self.user_id, **params)
         await TasksLayer.build(self._request.protocol, None, **params)
 
     async def _drop(self):
         """Exit channel"""
         params = {'member_id': self.user_id, 'channel_id': self._request.params.id}
-        sql.exit_channel(**params)
+        ChannelQueryManager.exit_channel(**params)
         await TasksLayer.build(self._request.protocol, None, **params)
 
     async def _delete(self):
         """Delete a channel"""
         pk = self._request.params.id
-        sql.delete_channel(self.user_id, pk)
+        ChannelQueryManager.delete_channel(self.user_id, pk)
         await TasksLayer.build(self._request.protocol, pk)

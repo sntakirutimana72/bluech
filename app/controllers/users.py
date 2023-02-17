3277f131171cfeeddee8ab0a2f2b34ce856d4020
Steven Ntakirutimana <sntakirutimana72@gmail.com>
2023-02-17 23:23:29 +0200

    Update controllers

--- app/controllers/users.py
+++ app/controllers/users.py
@@ -1,20 +1,19 @@
 from .base import Base
-from ..utils import sql
+from ..utils.sql import UserQueryManager
 from ..utils.layers import TasksLayer
 
 class Users(Base):
-
     async def _patch(self):
         """Edit user display name"""
         pk = self.user_id
-        sql.edit_user_display_name(pk, self._request.body.display_name)
+        UserQueryManager.edit_user_display_name(pk, self._request.body.display_name)
         await TasksLayer.build(self._request.protocol, pk)
 
     async def _put(self):
         """Change user profile picture"""
         pk = self.user_id
         body = self._request.body
-        sql.edit_user_profile_picture(pk, body.data, body.extension)
+        UserQueryManager.edit_user_profile_picture(pk, body.data, body.extension)
         await TasksLayer.build(self._request.protocol, pk)
 
     async def _get(self):

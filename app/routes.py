ae0cf633450731f68771d756312d7caa19791768
Steve <sntakirutimana72@gmail.com>
2023-02-19 21:25:06 +0200

    Modify routing system

--- app/routes.py
+++ app/routes.py
@@ -3,20 +3,11 @@ from .controllers import *
 
 route_patterns = [
     # session
-    router(('signin', 'signout',), SessionController),
+    router('signin', SessionController),
     # messages
-    router(
-        ('new_message', 'all_messages', 'edit_message', 'remove_message',),
-        MessagesController
-    ),
-    # users
-    router(
-        ('edit_username', 'edit_profile_pic', 'all_users',),
-        UsersController
-    ),
-    # groups
-    router(
-        ('new_group', 'new_member', 'remove_member', 'exit_group', 'remove_group', 'assign_group_privilege',),
-        ChannelsController
-    ),
+    # router('messages', MessagesController),
+    # # users
+    # router('users', UsersController),
+    # # groups
+    # router('channels', ChannelsController),
 ]
--- app/utils/router.py
+++ app/utils/router.py
@@ -3,9 +3,5 @@ from typing import Any, Callable
 from .interfaces import AttributeDict
 from ..settings import ALLOWED_ROUTES
 
-def router(args: str | tuple | list, controller: Callable) -> AttributeDict[str, Any] | list[AttributeDict[str, Any]]:
-    if isinstance(args, (list, tuple,)):
-        return [
-            router(name, controller) for name in args
-        ]
-    return AttributeDict({'url': ALLOWED_ROUTES[args], 'controller': controller})
+def router(name: str, controller: Callable) -> AttributeDict[str, Any]:
+    return AttributeDict({'url': ALLOWED_ROUTES[name], 'controller': controller})

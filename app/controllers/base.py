4240c195139ec778c55759fa575c22f805ab35a3
Steve <sntakirutimana72@gmail.com>
2023-02-19 21:26:03 +0200

    Update base & session controllers

--- app/controllers/base.py
+++ app/controllers/base.py
@@ -5,17 +5,17 @@ from ..utils.interfaces import Request
 class Base(object):
     def __init__(self, request: Request):
         self._request = request
-        
+
     @property
     def user_id(self) -> int:
         return self.session.user_id
-    
+
     @property
     def session(self):
         return self._request.session
 
     def _executor(self):
-        exec_name = f'_{self._request.method.lower()!r}'
+        exec_name = f'_{self._request.method.lower()}'
         return getattr(self, exec_name)
 
     def exec(self) -> asyncio.Future:
--- app/controllers/session.py
+++ app/controllers/session.py
@@ -2,11 +2,10 @@ from .base import Base
 from ..utils.sql import SessionQueryManager
 
 class Session(Base):
-
     async def _post(self):
         """ Signin """
         return SessionQueryManager.signin(**self._request.body.user)
-        
+
     async def _delete(self):
         """ Signout """
         SessionQueryManager.signout(self.user_id)

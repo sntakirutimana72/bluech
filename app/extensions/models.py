b17c1039d435866634a797653c4082b98585526f
Steve <sntakirutimana72@gmail.com>
2023-02-19 21:24:09 +0200

    Refactor & move models' extensions.

---
+++ app/extensions/models.py
@@ -0,0 +1,49 @@
+__all__ = (
+    'ChannelExtension',
+    'UserExtension',
+    'MemberExtension',
+    'MessageExtension',
+    'ResourceExtension',
+    'ActivityExtension',
+    'ActivityLogExtension',
+)
+
+import datetime as dt
+
+from ..policies.secure_password import PasswordHasherPolicy
+
+# noinspection PyAttributeOutsideInit
+class Extension(object):
+    def save(self, *args, **kwargs):
+        if self._pk:
+            self.updated_at = dt.datetime.now()
+        return super(Extension, self).save(*args, **kwargs)
+
+# noinspection PyAttributeOutsideInit
+class UserExtension(Extension):
+    def authenticate(self, password: str):
+        assert PasswordHasherPolicy.verify(password, self.password)
+
+    def save(self, *args, **kwargs):
+        if self.is_dirty() and 'password' in self.dirty_fields:
+            hashed_password = PasswordHasherPolicy.generate(self.password)
+            self.password = hashed_password
+        return super(UserExtension, self).save(*args, **kwargs)
+
+class ChannelExtension(Extension):
+    ...
+
+class MessageExtension(Extension):
+    ...
+
+class MemberExtension(Extension):
+    ...
+
+class ResourceExtension(Extension):
+    ...
+
+class ActivityExtension(Extension):
+    ...
+
+class ActivityLogExtension(Extension):
+    ...
--- app/utils/extensions/models.py
+++
@@ -1,6 +0,0 @@
-import peewee as pee
-
-class UserExtension(metaclass=pee.Model):
-    def authenticate(self, password: str):
-        if self.password != password:
-            raise

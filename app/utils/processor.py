ac79c861d07508ed22319a671f527d4e6056ced1
sntakirutimana72 <sntakirutimana72@gmail.com>
2023-02-20 23:12:17 +0200

    Adjust functionalities to compensate signout

--- app/utils/processor.py
+++ app/utils/processor.py
@@ -17,56 +17,67 @@ class Processor:
         self._session: AttributeDict | None = None
         self._request: Request | None = None
 
-        io.create_task(self._in_stream())
+        io.create_task(self.gather())
 
-    async def _in_registry(self, user):
-        self._session = AttributeDict({'user_id': user.id, 'is_group': False})
-        channel_layer = ChannelLayer(self._writer, user)
+    async def subscribe(self, user):
+        user_id = user.id
+        self._session = AttributeDict({'user_id': user_id, 'is_group': False})
+        channel_layer = ChannelLayer(self._writer, user_id)
 
         await RepositoriesHub.channels_repository.push(channel_layer)
         await PipeLayer.pump(self._writer, Response.signin_success(user))
-        await TasksLayer.build('users', user.id)
+        await TasksLayer.build('users', user_id)
+        
+    async def unsubscribe(self):
+        user_id = self._session.user_id
+        self._session = None
+        
+        await RepositoriesHub.channels_repository.remove(user_id)
+        await PipeLayer.pump(self._writer, Response.signout_success())
+        await TasksLayer.build('remove_user', user_id)
 
-    def _in_submission(self, req):
-        self._in_process(req)
+    def resolve(self, request):
+        self.sanitize(request)
         return dispatch(self._request).exec()
 
-    async def _in_transition(self, req):
+    async def process(self, request):
         try:
-            result = await self._in_submission(req)
+            result = await self.resolve(request)
             if self._session is None:
-                await self._in_registry(result)
+                await self.subscribe(result)
+            elif self._request.protocol == 'signout':
+                await self.unsubscribe()
         except CustomException as e:
             await PipeLayer.pump(self._writer, Response.signin_failure(**e.resp))
         except:
             await PipeLayer.pump(self._writer, Response.exception())
         self._request = None
 
-    async def _in_stream(self):
+    async def gather(self):
         try:
             while True:
-                req = await PipeLayer.fetch(self._reader)
-                await self._in_transition(req)
+                request = await PipeLayer.fetch(self._reader)
+                await self.process(request)
         except:
             ...
 
-    def _in_process(self, req):
+    def sanitize(self, request):
         try:
-            validated_req = AttributeDict(Validators.request(req))
+            validated_req = AttributeDict(Validators.request(request))
             action_req = validated_req.pop('request')
             proto = validated_req.pop('protocol')
 
             if self._session is None:
                 if proto != 'signin':
                     raise ProtocolValidationError
-                req = Validators.signin(action_req)
+                request = Validators.signin(action_req)
             elif proto not in ALLOWED_ROUTES:
                 raise ProtocolLookupError
             else:
                 validator = getattr(Validators, proto)
-                req = validator(action_req)
+                request = validator(action_req)
 
-            validated_req |= req
+            validated_req |= request
             validated_req.session = self._session  # pass down the session context
             self._request = Request(ALLOWED_ROUTES[proto], validated_req)
         except sc.SchemaError:

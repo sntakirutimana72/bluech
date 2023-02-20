b965811a465f3c3eccae960422ad5073be18583c
Steve <sntakirutimana72@gmail.com>
2023-02-20 10:49:48 +0200

    Test signin_success protocol

--- app/tests/controllers/test_session.py
+++ app/tests/controllers/test_session.py
@@ -1,33 +1,47 @@
 import pytest
 
 from ..support.unittests import PyTestCase
+from ..support.models import InstantUse
 from ..support.mocks.clients import AppClientSpec
 from ..support.declarations import RequestSpecs
 
 @pytest.mark.usefixtures('server')
-class TestSigninFailure(PyTestCase):
-    async def assertResponse(self, status, **user):
-        # connect to server
-        client = AppClientSpec()
-        await client.connect()
-        # formatting data and packing it for a specific factory pattern recognizable by the server
-        await client.send(RequestSpecs.signin(**user))
-        # now, receive server response
-        resp = await client.receive()
+class SessionTestCases(PyTestCase):
+    client: AppClientSpec
 
+    async def assertResponse(self, status: int, proto: str, **user: dict[str, str]):
+        resp = await self.signin(**user)
         self.assert_isinstanceof(resp, dict)
         self.assert_dict_has_key(resp, 'proto')
-        self.assert_equals(resp['proto'], 'signin_failure')
+        self.assert_equals(resp['proto'], proto)
         self.assert_dict_has_key(resp, 'status')
         self.assert_equals(resp['status'], status)
 
-        await client.disconnect()
+    async def signin(self, **credentials: dict[str, str]):
+        # connect to server
+        self.client = await AppClientSpec().connect()
+        # formatting data and packing it for a specific factory pattern recognizable by the server
+        await self.client.send(RequestSpecs.signin(**credentials))
+        # now, receive server response
+        resp = await self.client.receive()
+        return resp
 
+class TestSigninFailure(SessionTestCases):
     @pytest.mark.asyncio
     async def test_bad_request(self):
-        await self.assertResponse(400)
+        await self.assertResponse(400, 'signin_failure')
+        await self.client.disconnect()
 
     @pytest.mark.asyncio
     async def test_unauthorized(self):
         user = {'email': 'admin@test', 'password': 'test@123'}
-        await self.assertResponse(401, **user)
+        await self.assertResponse(401, 'signin_failure', **user)
+        await self.client.disconnect()
+
+class TestSigninSuccess(SessionTestCases):
+    @pytest.mark.asyncio
+    async def test_success(self):
+        with InstantUse.admin_user() as admin:
+            credentials = {'email': admin.email, 'password': 'test@123'}
+            await self.assertResponse(200, 'signin_success', **credentials)
+            await self.client.disconnect()

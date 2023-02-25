217b5438797c25ba6b877a1ef8c0d5efcf9d3748
Steve <sntakirutimana72@gmail.com>
2023-02-25 23:58:04 +0200

    Update user controller

--- app/controllers/users.py
+++ app/controllers/users.py
@@ -6,16 +6,16 @@ class Users(Base):
     async def _patch(self):
         """Edit user nickname"""
         pk = self.user_id
-        UserQueryManager.edit_nickname(pk, self._request.body.user.nickname)
-        await TasksLayer.build('nickname_edited', pk)
+        UserQueryManager.edit_nickname(pk, self.request.body.user.nickname)
+        await TasksLayer.build('edit_username_success', pk)
 
     async def _put(self):
         """Change user profile picture"""
         pk = self.user_id
-        body = self._request.body
+        body = self.request.body
         UserQueryManager.edit_user_profile_picture(pk, body.data, body.extension)
-        await TasksLayer.build(self._request.protocol, pk)
+        await TasksLayer.build(self.request.protocol, pk)
 
     async def _get(self):
         """Fetch all current users and groups that you're associated to"""
-        await TasksLayer.build(self._request.protocol, self.user_id)
+        await TasksLayer.build(self.request.protocol, self.user_id)

26f019c0a65ef02a73a2af6bd3c70c37324360cc
Steve <sntakirutimana72@gmail.com>
2023-02-26 00:00:54 +0200

    Create utils/emitter.py

---
+++ app/utils/emitter.py
@@ -0,0 +1,46 @@
+import asyncio as io
+
+from .layers import Response, ChannelLayer
+from .repositories import RepositoriesHub as Hub
+
+class Filters:
+    @staticmethod
+    def other_participants(except_recently):
+        for channel in Hub.channels_repository:
+            if channel.is_writable and channel.uid != except_recently:
+                yield channel
+
+class Responder:
+    @classmethod
+    def get_handler(cls, name: str):
+        return getattr(cls, name)
+
+    @staticmethod
+    async def connected(**options):
+        ...
+
+    @staticmethod
+    async def disconnected(**options):
+        ...
+
+    @staticmethod
+    async def edit_username(**options):
+        with Hub.tasks_repository.mutex:
+            with Hub.channels_repository.mutex:
+                channel: ChannelLayer = Hub.channels_repository.items.get(options['id'])
+                if channel is None:
+                    return
+                response = Response.edit_username_success(channel.resource)
+
+    @classmethod
+    async def pulse(cls):
+        while 1:
+            try:
+                task = await Hub.tasks_repository.fetch()
+                if task:
+                    proto = task.prop('proto')
+                    handler = cls.get_handler(proto)
+                    await handler(**task)
+            except:
+                ...
+            await io.sleep(1.25)

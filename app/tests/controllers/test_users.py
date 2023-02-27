da4e7eef4cffff6d79859cd8d6e13fcbeb3dc522
sntakirutimana72 <sntakirutimana72@gmail.com>
2023-02-27 23:46:08 +0200

    Add test units for changing user avatar

--- app/tests/controllers/test_users.py
+++ app/tests/controllers/test_users.py
@@ -1,9 +1,10 @@
 import pytest
+import asyncio as io
 
 from .conftest import ControllerTestCases
 
 @pytest.mark.usefixtures('user')
-class TestEditNickname(ControllerTestCases):
+class TestChangeUsername(ControllerTestCases):
     @pytest.mark.asyncio
     async def test_failure(self):
         # signing in
@@ -22,3 +23,9 @@ class TestEditNickname(ControllerTestCases):
         self.assertResponse(200, 'edit_username_success', resp)
         self.assert_dict_has_key(resp, 'user')
         await self.client.disconnect()
+        
+@pytest.mark.usefixtures('user')
+class TestChangeAvatar(ControllerTestCases):
+    @pytest.mark.asyncio
+    async def test_with_bad_request(self):
+        await self.assertSigninSuccess(email=self.user.email, password='test@123')

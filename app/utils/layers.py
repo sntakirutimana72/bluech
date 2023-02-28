8ded4b00eef23189e3f7e0ded78df2af20b83357
sntakirutimana72 <sntakirutimana72@gmail.com>
2023-03-01 00:44:56 +0200

    Add download logic in PipeLayer

--- app/utils/layers.py
+++ app/utils/layers.py
@@ -1,5 +1,8 @@
 import asyncio as io
 import typing as yi
+import pathlib as plib
+import aiofiles as aio
+import aiofiles.os as aios
 
 from .repositories import RepositoriesHub
 from .interfaces import AttributeDict
@@ -63,9 +66,53 @@ class Response:
 
 class PipeLayer:
     @staticmethod
+    def get_download_path(parent_dir: plib.Path, file_path: str):
+        complete_path = parent_dir / file_path
+        is_overwrite = complete_path.exists()
+        if is_overwrite:
+            current_stem = complete_path.stem
+            complete_path = complete_path.with_stem(f'{current_stem}.copy')
+        return complete_path, complete_path.exists()
+    
+    @staticmethod
+    def download_buffer(overal_size: int, buffer=1024):
+        return overal_size if overal_size < buffer else buffer
+    
+    @classmethod
+    async def download(cls, pipe: io.StreamReader, **options):
+        content_size: int = options['content_size']
+        remaining_content_size = content_size
+        buffer_size = cls.download_buffer(content_size)
+        download_path, is_overwrite = cls.get_download_path(plib.Path('~'), '')
+        # download_retrial = 5
+        try:
+            async with aio.open(download_path, 'wb') as pointer:
+                while remaining_content_size > 0:
+                    chunck = await pipe.read(buffer_size)
+                    await pointer.write(chunck)
+                    remaining_content_size -= buffer_size
+                    if buffer_size > remaining_content_size:
+                        buffer_size = remaining_content_size
+        except:
+            if is_overwrite and download_path.exists():
+                await aios.remove(download_path)
+        else:
+            if is_overwrite:
+                original_stem = download_path.stem.rstrip('.copy')
+                actual_path = download_path.with_stem(original_stem)
+                await aios.remove(actual_path)  # remove pre-existing first
+                await aios.rename(download_path, actual_path)  # rename the copy as the original
+                
+    @classmethod
+    async def download_avatar(cls, pipe: io.StreamReader, **options):
+        profile_images_path = './assets/profile/images'
+        is_done = await cls.download(pipe, parent_dir=profile_images_path, **options)
+        return is_done
+        
+    @staticmethod
     async def fetch(reader: io.StreamReader):
         content_size = await reader.read(4)
-        buffer_size = 1028
+        buffer_size = 1024
         content_size = int(content_size.decode())
 
         if content_size < buffer_size:

2dfd2ed8949b632ed329b9a2f6fca36c40e2b665
sntakirutimana72 <sntakirutimana72@gmail.com>
2023-03-07 20:34:54 +0200

    Update commons.py functinalities

--- app/utils/commons.py
+++ app/utils/commons.py
@@ -1,5 +1,12 @@
-def get_attribute_values(abs_obj, *attributes: tuple[str]):
-    yield from map(abs_obj, attributes)
+def get_attribute_values(abs_obj, only=[], exceptions=[]):
+    if not only:
+        only = abs_obj.__all__
+    if exceptions:
+        only = filter_with_exc(only, exceptions)
+    yield from map(abs_obj.__dict__.get, only)
     
-def filter_dict_items(dict_object, *attributes):
-    return dict((key, value) for key, value in dict_object.iteritems() if key in attributes)
+def filter_dict_items(dict_obj, only=[]):
+    return dict(filter(lambda els: els[0] in only, dict_obj.iteritems()))
+
+def filter_with_exc(original_iter, exceptions=[]):
+    return filter(lambda k: k not in exceptions, original_iter)

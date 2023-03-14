c27a0ea816007ee02eeece0cb87f2e00d5b30b8d
Steve <sntakirutimana72@gmail.com>
2023-03-15 01:56:16 +0200

    Add test unit for :remove_message routines

--- app/tests/controllers/test_messages.py
+++ app/tests/controllers/test_messages.py
@@ -1,4 +1,5 @@
 import pytest
+import asyncio as io
 
 from .conftest import ControllerTestCases
 from ..support.mocks.clients import AppClientSpec
@@ -21,6 +22,9 @@ class TestMessagesController(ControllerTestCases):
         delattr(cls, 'user')
         delattr(cls, 'rec_user')
 
+    def tmp_message(self):
+        return create_message(recipient=self.user, sender=self.rec_user, description='Hola!')
+
     async def affirmSigns(self):
         if self.client.connected:
             return
@@ -36,24 +40,35 @@ class TestMessagesController(ControllerTestCases):
         self.assert_equals(resp['message']['is_edited'], edited)
         self.assert_true(Message.select().exists())
 
+    def assertRemove(self, user_id, msg_id, resp):
+        self.assert_true(Skeletons.remove_message(resp))
+        self.assertResponse(200, 'remove_message', resp)
+        self.assert_equals(resp['message_id'], msg_id)
+        self.assert_equals(resp['benefactor']['id'], user_id)
+
     @pytest.mark.asyncio
     async def test_new_message_success(self):
         await self.affirmSigns()
-        # send a message
         await self.client.post_message('new_message', recipient=self.rec_user.id)
-        # receive message
         resp = await self.rec_client.receive()
-        # Then, assert
         self.assertMessageResponse('new_message', False, self.user, resp)
 
     @pytest.mark.asyncio
     async def test_edit_message_success(self):
         await self.affirmSigns()
-        # create a demo message
-        tmp_msg = create_message(recipient=self.user, sender=self.rec_user, description='Hola!')
-        # send a message
-        await self.rec_client.post_message('edit_message', params={'id': tmp_msg.id}, description='Salut!')
-        # receive message
+        msg = self.tmp_message()
+        await self.rec_client.post_message('edit_message', params={'id': msg.id}, description='Salut!')
         resp = await self.client.receive()
-        # Then, assert
         self.assertMessageResponse('edit_message', True, self.rec_user, resp)
+
+    @pytest.mark.asyncio
+    async def test_remove_message_success(self):
+        await self.affirmSigns()
+        msg_id = self.tmp_message().id
+        await self.rec_client.post_message('remove_message', params={'id': msg_id})
+        for client, user in ((self.rec_client, self.user), (self.client, self.rec_user)):
+            resp = await client.receive()
+            self.assertRemove(user.id, msg_id, resp)
+        msg = Message.get_or_none(Message.id == msg_id)
+        self.assert_is_none(msg)
+
